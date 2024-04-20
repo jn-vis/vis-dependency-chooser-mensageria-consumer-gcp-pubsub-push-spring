c7aa059604363d2e0ecc02071289f67f6540f319
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-20 19:39:13 -0300

    .

---
+++ src/main/java/com/ccp/jn/vis/topic/consumer/pubsub/push/application/VisGcpPubSubPushApplicationStarter.java
@@ -0,0 +1,75 @@
+package com.ccp.jn.vis.topic.consumer.pubsub.push.application;
+
+
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.decorators.CcpStringDecorator;
+import com.ccp.dependency.injection.CcpDependencyInjection;
+import com.ccp.exceptions.process.CcpAsyncProcess;
+import com.ccp.implementations.db.bulk.elasticsearch.CcpElasticSerchDbBulk;
+import com.ccp.implementations.db.dao.elasticsearch.CcpElasticSearchDao;
+import com.ccp.implementations.db.query.elasticsearch.CcpElasticSearchQueryExecutor;
+import com.ccp.implementations.db.utils.elasticsearch.CcpElasticSearchDbRequest;
+import com.ccp.implementations.email.sendgrid.CcpSendGridEmailSender;
+import com.ccp.implementations.file.bucket.gcp.CcpGcpFileBucket;
+import com.ccp.implementations.http.apache.mime.CcpApacheMimeHttp;
+import com.ccp.implementations.instant.messenger.telegram.CcpTelegramInstantMessenger;
+import com.ccp.implementations.json.gson.CcpGsonJsonHandler;
+import com.ccp.jn.async.business.JnAsyncBusinessNotifyError;
+import com.ccp.jn.vis.async.business.factory.CcpVisAsyncBusinessFactory;
+import com.jn.commons.entities.JnEntityAsyncTask;
+
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/{topic}")
+@SpringBootApplication
+public class VisGcpPubSubPushApplicationStarter {
+
+	public static void main(String[] args) {
+		CcpDependencyInjection.loadAllDependencies( 
+				new CcpElasticSearchQueryExecutor(),
+				new CcpTelegramInstantMessenger(),
+				new CcpVisAsyncBusinessFactory(),
+				new CcpElasticSearchDbRequest(),
+				new CcpSendGridEmailSender(),
+				new CcpElasticSerchDbBulk(),
+				new CcpElasticSearchDao(),
+				new CcpGsonJsonHandler(),
+				new CcpApacheMimeHttp(),
+				new CcpGcpFileBucket()  
+				);
+		SpringApplication.run(VisGcpPubSubPushApplicationStarter.class, args);
+	}
+	@PostMapping
+	public void onReceiveMessage(@PathVariable("topic") String topic, @RequestBody Map<String, Object> body) {
+		CcpJsonRepresentation ccpMapDecorator = new CcpJsonRepresentation(body);
+		CcpJsonRepresentation internalMap = ccpMapDecorator.getInnerJson("message");
+		String data = internalMap.getAsString("data");
+		String str = new CcpStringDecorator(data).text().asBase64();
+		CcpJsonRepresentation json = new CcpJsonRepresentation(str);
+		JnEntityAsyncTask entity = new JnEntityAsyncTask();
+		JnAsyncBusinessNotifyError jnAsyncBusinessNotifyError = new JnAsyncBusinessNotifyError();
+		CcpAsyncProcess.executeProcess(topic, json, entity, jnAsyncBusinessNotifyError);
+	}
+
+	@PostMapping("/testing")
+	public void onReceiveMessageTesting(@PathVariable("topic") String topic, @RequestBody Map<String, Object> json) {
+		CcpJsonRepresentation md = new CcpJsonRepresentation(json);
+		JnAsyncBusinessNotifyError jnAsyncBusinessNotifyError = new JnAsyncBusinessNotifyError();
+		CcpAsyncProcess.executeProcess(topic, md, jnAsyncBusinessNotifyError);
+	}
+
+}
