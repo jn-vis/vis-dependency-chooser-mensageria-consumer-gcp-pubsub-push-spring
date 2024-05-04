4da2c06f0707e8b5e69b39f724638625d37b4c15
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-04 00:23:56 -0300

    .

--- src/main/java/com/ccp/jn/vis/topic/consumer/pubsub/push/application/VisGcpPubSubPushApplicationStarter.java
+++ src/main/java/com/ccp/jn/vis/topic/consumer/pubsub/push/application/VisGcpPubSubPushApplicationStarter.java
@@ -19,7 +19,7 @@ import com.ccp.decorators.CcpStringDecorator;
 import com.ccp.dependency.injection.CcpDependencyInjection;
 import com.ccp.exceptions.process.CcpAsyncTask;
 import com.ccp.implementations.db.bulk.elasticsearch.CcpElasticSerchDbBulk;
-import com.ccp.implementations.db.dao.elasticsearch.CcpElasticSearchDao;
+import com.ccp.implementations.db.crud.elasticsearch.CcpElasticSearchCrud;
 import com.ccp.implementations.db.query.elasticsearch.CcpElasticSearchQueryExecutor;
 import com.ccp.implementations.db.utils.elasticsearch.CcpElasticSearchDbRequest;
 import com.ccp.implementations.email.sendgrid.CcpSendGridEmailSender;
@@ -46,7 +46,7 @@ public class VisGcpPubSubPushApplicationStarter {
 				new CcpElasticSearchDbRequest(),
 				new CcpSendGridEmailSender(),
 				new CcpElasticSerchDbBulk(),
-				new CcpElasticSearchDao(),
+				new CcpElasticSearchCrud(),
 				new CcpGsonJsonHandler(),
 				new CcpApacheMimeHttp(),
 				new CcpGcpFileBucket()  
